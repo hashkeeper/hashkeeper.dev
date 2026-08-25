df3a5c6ac14eacf5540b3df028f21296acef7a58
Edward Cronin <eddie@hashkeeper.dev>
2026-08-24 21:12:54 -0400

    Scroll Manager class fixes

--- js/scrollManager.js
+++ js/scrollManager.js
@@ -10,7 +10,7 @@ export class ScrollManager {
     this.header = document.querySelector("header");
     this.bufferOne = document.querySelector("#bufferOne");
     this.bufferTwo = document.querySelector("#bufferTwo");
-    this.headerBuffer = document.querySelector("#bufferHeader");
+    this.bufferHeader = document.querySelector("#bufferHeader");
 
     this.soundbite = document.querySelector("#soundbite");
 
@@ -20,9 +20,7 @@ export class ScrollManager {
   #init() {
     this.headerObserver.observe(this.bufferOne);
     this.headerObserver.observe(this.bufferTwo);
-    this.bodyObserver.observe(this.header);
-
-    this.nodeObj.body.scroll(0, 15);
+    this.bodyObserver.observe(this.bufferHeader);
   }
 
   headerObserver = new IntersectionObserver(
@@ -36,17 +34,13 @@ export class ScrollManager {
         }
       } else if (entry.target === this.bufferTwo) {
         if (entry.isIntersecting) {
-          this.hkHeader.style.opacity = "0";
-          this.header.style.transform = "translateY(-100%)";
           this.nodeObj.body.style.overflow = "scroll";
-          this.soundbite.focus();
-          this.nodeObj.body.style.overflow = "hidden";
-          setTimeout(() => {
-            this.nodeObj.body.style.overflow = "scroll";
-          }, 2000);
+          this.soundbite.focus({ preventScroll: true });
+
           this.headerObserver.disconnect();
-        } else {
-          this.nodeObj.body.style.overflow = "hidden";
+
+          this.header.style.display = "none";
+          this.hkHeader.style.opacity = "0";
         }
       }
     })},
@@ -59,20 +53,24 @@ export class ScrollManager {
 
   bodyObserver = new IntersectionObserver(
     (entries) => { entries.forEach((entry) => {
+      console.log(entry);
       if (entry.isIntersecting) {
-        this.bufferOne.focus();
+        this.nodeObj.body.style.overflow = "hidden";
+
+        this.header.style.display = "";
+        this.header.scrollTo(0, 0);
+        this.hkHeader.style.opacity = "1";
+
         this.headerObserver.observe(this.bufferOne);
         this.headerObserver.observe(this.bufferTwo);
-        // this.hkSplash.style.maskPosition = "0% 0%";
-        // this.hkHeader.style.opacity = "1";
-        // this.nodeObj.body.style.overflow = "hidden";
-        // this.header.style.transform = "translateY(0%)";
-        // this.nodeObj.nav.style.opacity = "0";
-      } else if (!entry.isIntersecting) {
-        this.header.style.transform = "translateY(-100%)";
-        setTimeout(() => {
-          this.nodeObj.nav.style.opacity = "1";
-        }, 1000);
+      } else {
+        this.headerObserver.disconnect();
+        this.header.style.display = "none";
+        this.hkSplash.style.maskPosition = "0% -100vh";
+        this.hkHeader.style.opacity = "0";
+          setTimeout(() => {
+            this.nodeObj.nav.style.opacity = "1";
+          }, 1000);
       }
     })},
     {
